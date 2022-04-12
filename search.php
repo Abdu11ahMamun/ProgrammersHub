0ae8a40117b4199898d0df30b23f72676e68ffe0
Sadman Sakib <natrium213@gmail.com>
2022-04-12 22:10:28 +0600

    searching word is fetched

--- search.php
+++ search.php
@@ -1,1 +1,11 @@
 <?php include'inc/header.php';?>
+<?php
+     if(!isset($_GET['search'])|| $_GET['search']==NULL){
+         header("Location: 404.php");
+     }else{
+         $search=$_GET['search'];
+     }
+?>
+
+<div class="contentsection contemplete clear">
+<div class="maincontent clear">
