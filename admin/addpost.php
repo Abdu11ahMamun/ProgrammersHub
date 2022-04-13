6bda16d7a5d8bb43d7b53a00d1f93c8192c717a0
abdu11ah-mamun <cs.abdullah.mamun@gmail.com>
2022-04-14 00:36:38 +0600

    addpost form added

--- admin/addpost.php
+++ admin/addpost.php
@@ -6,6 +6,82 @@
             
 
         </div>
+        <div class="block">               
+                 <form action="" method="post" enctype="multipart/form-data">
+                    <table class="form">
+                       
+                        <tr>
+                            <td>
+                                <label>Title</label>
+                            </td>
+                            <td>
+                                <input type="text" name="title" placeholder="Enter Post Title..." class="medium" />
+                            </td>
+                        </tr>
+                     
+                        <tr>
+                            <td>
+                                <label>Category</label>
+                            </td>
+                            <td>
+                                <select id="select" name="cat">
+                                    <option value="">Select Category</option>
+                                    <?php 
+                                        $query= "SELECT * from tbl_category";
+                                        $category= $db->select($query);
+                                        if($category){
+                                            while($result= $category->fetch_assoc()){
+
+                                    ?>
+                                    <option value="<?php echo $result['id']; ?>"><?php echo $result['name']; ?></option>
+                                    <? }} ?>
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label>Upload Image</label>
+                            </td>
+                            <td>
+                                <input type="file" name="image" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="vertical-align: top; padding-top: 9px;">
+                                <label>Content</label>
+                            </td>
+                            <td>
+                                <textarea class="tinymce" name="body" ></textarea>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>
+                                <label>Tags</label>
+                            </td>
+                            <td>
+                                <input type="text" name="tags" placeholder="Enter tags..." class="medium" />
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>
+                                <label>Author</label>
+                            </td>
+                            <td>
+                                <input type="text" name="author" placeholder="Enter Author name..." class="medium" />
+                            </td>
+                        </tr>
+
+						<tr>
+                            <td></td>
+                            <td>
+                                <input type="submit" name="submit" Value="Save" />
+                            </td>
+                        </tr>
+                    </table>
+                    </form>
+                </div>
 
     </div>
 
