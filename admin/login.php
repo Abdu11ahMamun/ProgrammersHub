8d56a7275084513b4dfab5d1ef846b38968d0091
Sadman Sakib <natrium213@gmail.com>
2022-04-13 21:38:24 +0600

    html form is created and button has been added

--- admin/login.php
+++ admin/login.php
@@ -9,3 +9,30 @@ Session::init(); //access in the session
      $db=new Database();
 	 $fm=new Format();
 ?>
+<!DOCTYPE html>
+<head>
+<meta charset="utf-8">
+<title>Login</title>
+    <link rel="stylesheet" type="text/css" href="css/stylelogin.css" media="screen" />
+</head>
+<body>
+<div class="container">
+	<section id="content">
+		<form action="login.php" method="post">
+			<h1>Admin Login</h1>
+			<div>
+				<input type="text" placeholder="Username" required="" name="username"/>
+			</div>
+			<div>
+				<input type="password" placeholder="Password" required="" name="password"/>
+			</div>
+			<div>
+				<input type="submit" value="Log in" />
+			</div>
+		</form><!-- form -->
+		<div class="button">
+		</div><!-- button -->
+	</section><!-- content -->
+</div><!-- container -->
+</body>
+</html>
