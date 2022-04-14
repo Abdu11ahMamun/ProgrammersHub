ec0ab36e947ad29d7df3660249947187c84097fb
abdu11ah-mamun <cs.abdullah.mamun@gmail.com>
2022-04-15 02:11:09 +0600

    add post query done with few errors

--- admin/addpost.php
+++ admin/addpost.php
@@ -4,7 +4,49 @@
 		
             <div class="box round first grid">
                 <h2>Add New Post</h2>
-                
+                <?php
+                   if ($_SERVER['REQUEST_METHOD']=='POST'){
+                        $title=mysqli_real_escape_string($db->link,$_POST['title']);
+                        $cat=mysqli_real_escape_string($db->link,$_POST['cat']);
+                        $body=mysqli_real_escape_string($db->link,$_POST['body']);
+                        $tags=mysqli_real_escape_string($db->link,$_POST['tags']);
+                        $author=mysqli_real_escape_string($db->link,$_POST['author']);
+
+
+                        //Image validation part
+                        $permited  = array('jpg', 'jpeg', 'png', 'gif');
+                        $file_name = $_FILES['image']['name'];
+                        $file_size = $_FILES['image']['size'];
+                        $file_temp = $_FILES['image']['tmp_name'];
+                    
+                        $div = explode('.', $file_name);
+                        $file_ext = strtolower(end($div));
+                        $unique_image = substr(md5(time()), 0, 10).'.'.$file_ext;
+                        $uploaded_image = "upload/".$unique_image;
+
+                        if ($title=="" || $cat=="" || $body=="" || $tags=="" || $author=="" || $file_name=="") 
+                        {
+                             echo "<span class='error >Field must not be empty ! </span>";
+                        } elseif ($file_size >1048567) {
+                             echo "<span class='error'>Image Size should be less then 1MB!
+                             </span>";
+                        } elseif (in_array($file_ext, $permited) === false) 
+                        {
+                             echo "<span class='error'>You can upload only:-"
+                             .implode(', ', $permited)."</span>";
+                        } else{
+                            move_uploaded_file($file_temp, $uploaded_image);
+                            $query = "INSERT INTO tbl_post(cat, title, body, image, author, tags) VALUES('$cat','$title','$body','$uploaded_image','$author','$tags')";
+                            $inserted_rows = $db->insert($query);
+                            if ($inserted_rows) {
+                             echo "<span class='success'>Post Inserted Successfully. </span>";
+                            }else{
+                             echo "<span class='success'>Post Not Inserted. </span>";
+                            }
+                        }
+                        
+                   }
+                ?>
                 <div class="block">               
                  <form action="" method="post" enctype="multipart/form-data">
                     <table class="form">
@@ -25,7 +67,15 @@
                             <td>
                                 <select id="select" name="cat">
                                     <option value="">Select Category</option>
-                                    
+                                    <?php 
+                                        $query= "SELECT * from tbl_category";
+                                        $category= $db->select($query);
+                                        if($category){
+                                            while($result= $category->fetch_assoc()){
+
+                                    ?>
+                                    <option value="<?php echo $result['id']; ?>"><?php echo $result['name']; ?></option>
+                                    <? }} ?>
                                 </select>
                             </td>
                         </tr>
@@ -75,6 +125,6 @@
                 </div>
             </div>
         </div>
-  
+
 
 <?php include 'inc/footer.php';?>
