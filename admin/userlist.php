1c94e247fb542bb20fca0a80a85d569131292e97
Sadman Sakib <natrium213@gmail.com>
2022-04-15 23:35:56 +0600

    table heading is added in the file

--- admin/userlist.php
+++ admin/userlist.php
@@ -1,5 +1,35 @@
 <?php include 'inc/header.php';?>
 <?php include 'inc/sidebar.php';?>
 
-        
+        <div class="grid_10">
+            <div class="box round first grid">
+                <h2>User List</h2>
+
+
+                <div class="block">
+                    <table class="data display datatable" id="example">
+					<thead>
+						<tr>
+							<th>Serial No.</th>
+							<th>Name</th>
+							<th>Username</th>
+							<th>Email</th>
+							<th>Details</th>
+							<th>Role</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					
+				</table>
+               </div>
+            </div>
+        </div>
+        <script type= "text/javascript">
+        $(document).ready(function()
+        {
+            setupLeftMenu();
+            $('.datatable').dataTable();
+            setSidebarHeight();
+        });
+        </script>
 <?php include 'inc/footer.php';?>
