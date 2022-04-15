63b4daa5995ea793f464f6936e26338fae4c5f92
Arefin Ittesafun Abian <Aabian191042@bscse.uiu.ac.bd>
2022-04-15 21:32:43 +0600

    catid is being found or not

--- admin/editcat.php
+++ admin/editcat.php
@@ -1,5 +1,17 @@
 <?php include 'inc/header.php';?>
 <?php include 'inc/sidebar.php';?>
+<?php
+if(!isset($_GET['catid']) || $_GET['catid'] == NULL)
+{
+    echo "<script>window.location = 'catlist.php'; </script>"; // if no id is found return to catlist page
+
+}
+else
+{
+    $id = $_GET['catid'];
+}
+
+?>
 
 
 <?php include 'inc/footer.php';?>
