085b4647782c1aed2722be41411c34d21aed03b6
Arefin Ittesafun Abian <Aabian191042@bscse.uiu.ac.bd>
2022-04-15 19:24:55 +0600

    html and javascript has been added

--- admin/catlist.php
+++ admin/catlist.php
@@ -1,21 +1,32 @@
 <?php include 'inc/header.php';?>
 <?php include 'inc/sidebar.php';?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<div class="grid_10">
+            <div class="box round first grid">
+                <h2>Category List</h2>
+                
+                <div class="block">        
+                    <table class="data display datatable" id="example">
+					<thead>
+						<tr>
+							<th>Serial No.</th>
+							<th>Category Name</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					<tbody>
+                       
+					</tbody>
+				</table>
+               </div>
+            </div>
+        </div>
+        <script type= "text/javascript">
+        $(document).ready(function()
+        {
+            setupLeftMenu();
+            $('.datatable').dataTable();
+            setSidebarHeight();
+        });
+        </script>
 
 <?php include 'inc/footer.php';?>
