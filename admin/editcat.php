432bb55c708d1b4d26bb179d075f20ca8df6275d
Arefin Ittesafun Abian <Aabian191042@bscse.uiu.ac.bd>
2022-04-15 21:43:36 +0600

    fetching query has been written

--- admin/editcat.php
+++ admin/editcat.php
@@ -12,6 +12,37 @@ else
 }
 
 ?>
-
+ <div class="grid_10">
+		
+        <div class="box round first grid">
+            <h2>Update Category</h2>
+           <div class="block copyblock"> 
+               
+               
+               <?php
+               //fetch the category
+               $query = "select * from tbl_category where id=$id order by id desc"; 
+               $category = $db-> select($query);
+               while ($result = $category->fetch_assoc())
+               {
+               ?>
+             <form action="" method="post">
+                <table class="form">					
+                    <tr>
+                        <td>
+                            <input type="text" name="name" value="<?php echo $result['name'];?>" class="medium" />
+                        </td>
+                    </tr>
+                    <tr> 
+                        <td>
+                            <input type="submit" name="submit" Value="Save" />
+                        </td>
+                    </tr>
+                </table>
+                </form>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
 
 <?php include 'inc/footer.php';?>
