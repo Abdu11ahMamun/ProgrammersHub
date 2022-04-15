c3a90854949d85392a2586719acb404bce842c61
Arefin Ittesafun Abian <Aabian191042@bscse.uiu.ac.bd>
2022-04-15 16:52:46 +0600

    checking category

--- posts.php
+++ posts.php
@@ -1,7 +1,13 @@
 <?php include'inc/header.php';?>
 <?php include'inc/sidebar.php'; ?>
 
-
+<?php
+     if(!isset($_GET['category'])|| $_GET['category']==NULL){
+         header("Location: 404.php");
+     }else{
+         $id=$_GET['category'];
+     }
+?>
 
 
 
