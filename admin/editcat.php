b03e5ae82d073d2cbfae44a0fb535bfd7b0a3ec9
Arefin Ittesafun Abian <Aabian191042@bscse.uiu.ac.bd>
2022-04-16 03:25:25 +0600

    html code is being written

--- admin/editcat.php
+++ admin/editcat.php
@@ -11,6 +11,31 @@ else
     $id = $_GET['catid'];
 }
 ?>
+<div class="grid_10">
+		
+        <div class="box round first grid">
+            <h2>Update Category</h2>
+           <div class="block copyblock"> 
+               
+               
+             <form action="" method="post">
+                <table class="form">					
+                    <tr>
+                        <td>
+                            <input type="text" name="name" value="<?php echo $result['name'];?>" class="medium" />
+                        </td>
+                    </tr>
+                    <tr> 
+                        <td>
+                            <input type="submit" name="submit" Value="Save" />
+                        </td>
+                    </tr>
+                </table>
+                </form>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
 
 
 
