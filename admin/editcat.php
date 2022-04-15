4d32cd57b3c8992b445267088e9b80779265478b
Arefin Ittesafun Abian <Aabian191042@bscse.uiu.ac.bd>
2022-04-16 03:22:56 +0600

    categoryid id being found or not

--- admin/editcat.php
+++ admin/editcat.php
@@ -1,5 +1,16 @@
 <?php include 'inc/header.php';?>
 <?php include 'inc/sidebar.php';?>
+<?php
+if(!isset($_GET['catid']) || $_GET['catid'] == NULL)
+{
+    echo "<script>window.location = 'catlist.php'; </script>"; // if no id is found return to catlist page
+
+}
+else
+{
+    $id = $_GET['catid'];
+}
+?>
 
 
 
